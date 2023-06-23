50c4d0ff845dc5e882454dbd9d6836a6aee4e098
Rajnish Singh Thakur <74904062+RajnishProgrammer@users.noreply.github.com>
2023-06-24 00:41:58 +0530

    Update settings.py

--- foodsearchapp/settings.py
+++ foodsearchapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-pkuyum2+r912cak4(s-v#381c&9y4z($9w-i0&+p@uc5&b%i%s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
